7528b639c7555911367f1c3e7b535d00e0298ea5
Henry Sztul <henry@sztul.com>
2011-12-06 17:41:23 -0500

    updated weekly harvester to calculate retention, weekly for now

--- weekly_activity_harvestr.rb
+++ weekly_activity_harvestr.rb
@@ -13,7 +13,7 @@ db = Mongo::Connection.new(MONGO_HOST, MONGO_PORT).db(DB_NAME)
 ####################################################
 map = <<-JS
       function(){
-        emit(this._id, {count: 1});
+        emit(this._id, {days_with_activity: 1});
       }
       JS
 
@@ -21,7 +21,7 @@ reduce = <<-JS
       function (key, values) {
         var sum = 0;
         values.forEach(function (f) {
-          sum += f.count;
+          sum += f.days_with_activity;
         });
         return {days_with_activity: sum};
       } 
@@ -45,10 +45,24 @@ end
 ####################################################
 # send number of weekly active users to StatsD
 ####################################################
-coll = db.collection(weekly_collection_name)
+weekly_coll = db.collection(weekly_collection_name)
 statsd = Statsd.new(STATSD_SERVER, STATSD_PORT)
-statsd.count('activity.weekly.total', coll.count)
+statsd.count('activity.weekly.total', weekly_coll.count)
 7.times do |i|
   i+=1
-  statsd.count('activity.weekly.visits.#{i}', coll.find("value.days_with_activity" => i).count)  
+  statsd.count('activity.weekly.visits.#{i}', weekly_coll.find("value.days_with_activity" => i).count)  
 end
+
+####################################################
+# calculate retention rate and send to StatsD
+####################################################
+day_1 = Date.today - 1
+day_2 = Date.today - 8
+coll_name_a = "Weekly:" + day_1.year.to_s + day_1.month.to_s + day_1.day.to_s
+coll_name_b = "Weekly:" + day_2.year.to_s + day_2.month.to_s + day_2.day.to_s
+coll_a = db.collection(coll_name_a)
+coll_b = db.collection(coll_name_b)
+# run map_reduce over both weeks collections and only keep those with value.count > 1 to get the logical AND
+coll_a.map_reduce(map, reduce, { :out => {:reduce => "retention:temp"} }) if coll_a.count > 0
+coll_b.map_reduce(map, reduce, { :out => {:reduce => "retention:temp"} }) if coll_b.count > 0
+db.collection("retention:temp").find( "value.days_with_activity" => {"$gte" => 2} ).count
